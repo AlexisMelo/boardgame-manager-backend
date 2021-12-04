edaa0820156dd1f8fe0c509bdfd6061dd4034268
AlexisMelo <Alexis.Robotiik@hotmail.fr>
2021-12-04 21:49:12 +0100

    ajout prise en compte pseudos

--- main.js
+++ main.js
@@ -17,8 +17,19 @@ app.use(express.static(path.join(__dirname, "views/")))
 
 app.use(express.json())
 
+let rooms = []
+
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username
+    if (!username) {
+        return next(new Error("invalid username"))
+    }
+    socket.username = username
+    next()
+})
+
 io.on("connection", (socket) => {
-    console.log("Nouvelle socket connectée !")
+    console.log(`Nouvelle socket de ${socket.username} connectée !`)
 
     socket.on("object-added", data => {
         socket.broadcast.emit("new-add", data)
@@ -27,7 +38,12 @@ io.on("connection", (socket) => {
     socket.on("object-modified", data => {
         socket.broadcast.emit("new-modification", data)
     })
+
+    socket.on("disconnect", () => {
+        console.log(`Socket ${socket.username} déconnectée `)
+    })
 })
+
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
